b0f2f2f93c135c16524336818eb8bc722ca39d0d
rares <roman_rares93@yahoo.com>
2021-02-02 01:47:11 +0200

    re-organize folders + small refactor

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+
+// inputs, buttons from login page
+const loginButton = document.getElementById("login-button");
+const usernameInput = document.getElementById("username-input");
+const passwordInput = document.getElementById("password-input");
+
+
+loginButton.addEventListener("click", () => {
+    alert("asd");
+});
--- js/registration.js
+++ js/registration.js
@@ -44,4 +44,3 @@ registerButton.addEventListener("click", () => {
     }
 
 });
-
