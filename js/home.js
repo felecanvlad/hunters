5814abead2c0a59998161adc0b0b7d6469d8aaea
rares <roman_rares93@yahoo.com>
2021-02-04 23:40:49 +0200

    display current page when navigating from left menu bar

--- js/home.js
+++ js/home.js
@@ -1,4 +1,34 @@
-var activePage = "home";
+let currentPage = "home";
+const ACTIVE_PROPERTY = "active";
 
-const home = document.getElementById("main");
-home.style.display = "";
+function hide(id) {
+    const elem = document.getElementById(id);
+    elem ? elem.style.display = "none" : console.error("Go away! Element does not exists!");
+}
+
+function hidePreviousPage() {
+    hide(currentPage);
+    const link = document.querySelector(`#left-menu-bar a[data-page="${currentPage}"]`);
+    link.classList.remove(ACTIVE_PROPERTY);
+}
+
+function showCard(pageId) {
+    hidePreviousPage();
+    document.getElementById(pageId).style.display = "";
+    const link = document.querySelector(`#left-menu-bar a[data-page="${pageId}"]`);
+    link.classList.add(ACTIVE_PROPERTY);
+    currentPage = pageId;
+}
+
+function initializeLeftMenu() {
+    document.addEventListener("click", function (e) {
+        const link = e.target;
+        if (link.matches("#left-menu-bar a")) {
+            const id = link.getAttribute("data-page");
+            showCard(id);
+        }
+    });
+}
+
+initializeLeftMenu();
+showCard(currentPage);
